81a908e42839f3177644a60df6550a9fbe4d9add
Mostafa <mos.sameti@gmail.com>
2020-05-03 19:51:02 +0430

    initial commit

---
+++ readme.md
@@ -0,0 +1,7 @@
+# simple react app boilerplate
+Author: Mostafa Sameti
+
+## Usage
+* `npm install` to install required packages.
+* `npm run dev` to run project on dev-server at localhost:8080.
+* `npm run build` to run project on dev-server at localhost:8080.
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: path.resolve(__dirname, "src/index"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, "src"),
+        use: ["babel-loader"],
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, "dist"),
+    port: 9000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "public/index.html", //source html
+    }),
+  ],
+};
