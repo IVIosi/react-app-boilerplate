2ee278c0afe161e31f970bd8c07cdea5f67fd662
Mostafa <mos.sameti@gmail.com>
2020-05-18 13:33:14 +0430

    linting webpack configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,28 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: path.resolve(__dirname, "src/index"),
+  entry: path.resolve(__dirname, 'src/index'),
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
   },
   module: {
     rules: [
       {
         test: /\.js$/,
-        include: path.resolve(__dirname, "src"),
-        use: ["babel-loader"],
+        include: path.resolve(__dirname, 'src'),
+        use: ['babel-loader'],
       },
     ],
   },
   devServer: {
-    contentBase: path.resolve(__dirname, "dist"),
+    contentBase: path.resolve(__dirname, 'dist'),
     port: 9000,
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "public/index.html", //source html
+      template: 'public/index.html', //source html
     }),
   ],
 };
